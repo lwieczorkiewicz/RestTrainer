bec330f50e72039bbd155ec8ad6347ccb95b0694
Łukasz Wieczorkiewicz <lwieczorkiewicz@gmail.com>
2021-03-10 18:39:38 +0100

    Initial commit

---
+++ src/main/java/com/example/RestTrainer/RestTrainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestTrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTrainerApplication.class, args);
+	}
+
+}
